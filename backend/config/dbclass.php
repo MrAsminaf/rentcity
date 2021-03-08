bff9a6563b52aee75b36441d105db1e4bbb55175
Adam Olgrzymek <mrasminaf@gmail.com>
2021-03-08 23:02:52 +0100

    Added initial project structure

---
+++ backend/config/dbclass.php
@@ -0,0 +1,32 @@
+<?php
+
+class DBClass {
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $database = ""; // fill the db name
+
+    public $connection;
+
+    public function GetConnection() {
+        $this->connection = null;
+
+        try {
+
+            $this->connection = new PDO(
+                "mysql:host-".$this->host
+                ."dbname=".$this->database
+                , $this->username
+                , $this->password);
+
+        } catch(PDOException $exception) {
+
+            echo "Error: ".$exception->getMessage();
+
+        }
+
+        return $this->connection;
+    }
+}
+
+?>
